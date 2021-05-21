efa25f683b87dcc0601c8a8d5ea52e1ab39d7076
Ilia Yusov <ouster2k@gmail.com>
2021-05-21 18:39:29 +0300

    refactor

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRE_URI') \
-        or 'postgresql://Ouster:1234@localhost:5432/flask_test_api'
-    SALT = os.environ.get('SALT') or 'my_sJHLHLHKLаваыпuper_s!alt_#4$4344'
