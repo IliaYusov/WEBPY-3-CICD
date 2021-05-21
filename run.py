ef2899fa84d3a2b2d11a8a09193e10d7d55bce35
Ilia Yusov <ouster2k@gmail.com>
2021-05-21 18:48:31 +0300

    refactor 2

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+
+from app import app, db
+from app.models import User, Advert
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Advert': Advert}
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=int(os.getenv('PORT', default=5000)),
+    )
